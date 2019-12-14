5251d357dab1552b333395c2dd2651ffb17f9908
Alexander Bittner <alexander.g.bittner@gmail.com>
2019-12-14 17:26:34 +0100

    skeleton program set up

---
+++ aestool.py
@@ -0,0 +1,41 @@
+print("test")
+
+def encrypt():
+    # TODO: encryption function
+    print("encryption function; not yet implemented")
+
+def decrypt():
+    # TODO: decryption function
+    print("decryption function; not yet implemented")
+
+def key_addition():
+    # TODO: implement key addition layer
+    print("key addition function; not yet implemented")
+
+def byte_substitution():
+    # TODO: implement byte_substitution layer
+    print("byte substitution function (running the S-Boxes)); not yet implemented")
+
+def shift_rows():
+    # TODO: implement shift_rows layer
+    print("shift rows function; not yet implemented")
+
+def mix_column():
+    # TODO: implement mix_column layer
+    print("mix column function; not yet implemented")
+
+def generate_subkeys():
+    # TODO: implement subkeys function that generates x subkeys from master key depending on length
+    print("subkey generation function; not yet implemented")
+
+def sbox():
+    # TODO: implement the s-box lookup table
+    print("s-box lookup function, not yet implemented")
+
+def g_function():
+    # TODO: implement g-function
+    print("g-function; not yet implemented")
+
+def h_function():
+    # TODO: implement h-function
+    print("h-function; not yet implemented")
